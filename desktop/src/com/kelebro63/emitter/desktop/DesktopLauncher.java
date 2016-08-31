350c3e7d419b3197f0c9d4b457e75c0592f9af42
Kelebro63 <kelebro63@gmail.com>
2016-08-31 15:40:40 +0300

    init commit

---
+++ desktop/src/com/kelebro63/emitter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kelebro63.emitter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kelebro63.emitter.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
